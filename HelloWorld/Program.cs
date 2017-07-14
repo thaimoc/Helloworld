576eb19f1047aca009986d3923c98c8ca52b4572
Đại Mộc Thanh Thiên <namcuongqp2@gmail.com>
2017-07-14 17:27:56 +0700

    Save change line 2

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,7 +10,9 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world!");
             Console.WriteLine("This is line 2");
+            Console.ReadKey();
         }
     }
 }
